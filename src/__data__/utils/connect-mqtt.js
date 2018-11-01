2fcdf22d77a30a5465e4ea8e2db706a2210cadc8
AllaDemidova <alla.v.demidova@mail.ru>
2018-11-01 19:08:20 +0300

    add notification action, save sensor`s data

--- src/__data__/utils/connect-mqtt.js
+++ src/__data__/utils/connect-mqtt.js
@@ -4,18 +4,15 @@ import {TOPIC, CONNECTION_URL, RECONNECT_TIMEOUT } from '../constants'
 import { MQTT_CONNECT_FAILURE, MQTT_CONNECT_SUCCESS, MQTT_SUBSCRIBE_SUCCESS, MQTT_SUBSCRIBE_FAILURE, MQTT_RECIEVE_MESSAGE } from '../action-types'
 
 export const connectMqtt = (dispatch) => {
-
-    debugger
     const client = mqtt.connect(CONNECTION_URL,
         {
             connectTimeout: RECONNECT_TIMEOUT,
             username: 'mqtt_client',
             password: 'sberhack',
-            protocolId: 'ws',
         })
 
     client.on('connect', function () {
-        debugger
+
         dispatch({ type: MQTT_CONNECT_SUCCESS })
 
         client.subscribe(TOPIC, function (err) {
@@ -26,13 +23,12 @@ export const connectMqtt = (dispatch) => {
     })
 
     client.on('message', function (topic, message) {
-        debugger
-        dispatch({ type: MQTT_RECIEVE_MESSAGE, topic, message })
+        const decodedMessage = new TextDecoder('utf-8').decode(message)
+        dispatch({ type: MQTT_RECIEVE_MESSAGE, topic, message: decodedMessage })
         //client.end()
     })
 
     client.on('error', function (error) {
-        debugger
         dispatch({ type: MQTT_CONNECT_FAILURE, error })
     })
 }
