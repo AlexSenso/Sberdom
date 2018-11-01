135728e2bfc1fd56b3f647b9305a253af0df380e
AllaDemidova <alla.v.demidova@mail.ru>
2018-11-01 19:33:49 +0300

    delete unused action

---
+++ src/__data__/reducers/notification.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+import { FETCH_NOTIFICATIONS_REQUET, FETCH_NOTIFICATION_SUCCESS, FETCH_NOTIFICATION_FAILURE} from '../action-types'
+import { STATUSES } from '../constants'
+
+export function notification(state = {}, action) {
+    switch (action.type) {
+        case FETCH_NOTIFICATIONS_REQUET:
+            return _.assign({}, state, { requestStatus: STATUSES.LOADING })
+        case FETCH_NOTIFICATION_SUCCESS:
+            return _.assign({}, state, { requestStatus: STATUSES.SUCCESS, notification: action.notification })
+        case FETCH_NOTIFICATION_FAILURE:
+            return _.assign({}, state, { requestStatus: STATUSES.FAILURE })
+    }
+}
