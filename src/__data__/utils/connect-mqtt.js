ff748e1d9df4a9e76fa0be1d25388a5ab2b0bc9b
AllaDemidova <alla.v.demidova@mail.ru>
2018-11-01 19:31:19 +0300

    last attempt

---
+++ src/__data__/utils/connect-mqtt.js
@@ -0,0 +1,34 @@
+import mqtt from 'mqtt'
+import _ from 'lodash'
+import {TOPIC, CONNECTION_URL, RECONNECT_TIMEOUT } from '../constants'
+import { MQTT_CONNECT_FAILURE, MQTT_CONNECT_SUCCESS, MQTT_SUBSCRIBE_SUCCESS, MQTT_SUBSCRIBE_FAILURE, MQTT_RECIEVE_MESSAGE } from '../action-types'
+
+export const connectMqtt = (dispatch) => {
+    const client = mqtt.connect(CONNECTION_URL,
+        {
+            connectTimeout: RECONNECT_TIMEOUT,
+            username: 'mqtt_client',
+            password: 'sberhack',
+        })
+
+    client.on('connect', function () {
+
+        dispatch({ type: MQTT_CONNECT_SUCCESS })
+
+        client.subscribe(TOPIC, function (err) {
+            const actionType = !_.isEmpty(err) ? MQTT_SUBSCRIBE_FAILURE : MQTT_SUBSCRIBE_SUCCESS
+
+            dispatch({ type: actionType })
+        })
+    })
+
+    client.on('message', function (topic, message) {
+        const decodedMessage = new TextDecoder('utf-8').decode(message)
+        dispatch({ type: MQTT_RECIEVE_MESSAGE, topic, message: decodedMessage })
+        //client.end()
+    })
+
+    client.on('error', function (error) {
+        dispatch({ type: MQTT_CONNECT_FAILURE, error })
+    })
+}
