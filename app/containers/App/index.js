9e96557060456ac041f9030c71bbc2c2025d202d
Alex <spreymetall@gmail.com>
2019-05-25 20:24:47 +0600

    Fixed layout

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -4,57 +4,23 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import Service from '../Service';
 import HomePage from '../HomePage/Loadable';
 
-import { LeftSidebar } from './components/LeftSidebar';
 import './App.scss';
 
 const AppWrapper = styled.div`
   min-height: 100%;
 `;
 
-const mainMenuItems = ['НОВОСТИ', 'СЕРВИС', 'SETTINGS'];
-const pages = [
-  HomePage,
-  Service,
-  HomePage,
-]
-
 class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeSlide: 0,
-    };
-  }
-
-  onMenuClick = event => {
-    event.preventDefault();
-    this.setState({ activeSlide: parseInt(event.target.dataset.key) });
-  };
-
   render() {
-    const page = pages[this.state.activeSlide]
-
     return (
       <AppWrapper>
         <Helmet titleTemplate="%s - Живой дом" defaultTitle="Живой дом" />
-        <div className="layout-wrapper">
-          <LeftSidebar
-            items={mainMenuItems}
-            onClick={this.onMenuClick}
-            activeSlide={this.state.activeSlide}
-            className="left-sidebar"
-          />
-          <div className="region-content">
-            <Switch>
-              <Route exact path="/" component={page} />
-              <Route path="" component={NotFoundPage} />
-            </Switch>
-          </div>
-        </div>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="" component={NotFoundPage} />
+          </Switch>
       </AppWrapper>
     );
   }
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,69 +1,56 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
-
 import injectReducer from 'utils/injectReducer';
 import injectSaga from 'utils/injectSaga';
-import {
-  makeSelectRepos,
-  makeSelectLoading,
-  makeSelectError,
-} from 'containers/App/selectors';
-import { loadRepos } from '../App/actions';
-import { changeUsername } from './actions';
-import {
-  addToCart,
-  stopAnimation,
-  startAnimation,
-} from '../ShoppingCart/actions';
-import Cart from '../../components/Cart';
-import {
-  makeSelectCatalog,
-  makeSelectUsername,
-  makeSelectFeedbacks,
-} from './selectors';
+
 import reducer from './reducer';
 import saga from './saga';
-import WhySection from './sections/WhySection';
-import ReviewsSection from './sections/ReviewsSection';
-import ProgramSection from './sections/ProgramSection';
-import ProductSection from './sections/ProductSection';
-import SweetsSection from './sections/SweetsSection';
-import FeedbacksSection from './sections/FeedbacksSection';
-import CallBackSection from './sections/CallBackSection';
 
 import './HomePage.scss';
-import { scrollTo } from '../../utils/ui-methods';
+import Service from '../Service';
+import { LeftSidebar } from '../App/components/LeftSidebar';
+
+const mainMenuItems = ['НОВОСТИ', 'СЕРВИС', 'SETTINGS'];
+const pages = [() => <div>Димас</div>, Service, () => <div>Лох</div>];
 
 /* eslint-disable react/prefer-stateless-function */
 export class HomePage extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      activeSlide: 0,
+    };
   }
 
+  onMenuClick = event => {
+    event.preventDefault();
+    this.setState({ activeSlide: parseInt(event.target.dataset.key) });
+  };
+
   render() {
+
+    const Page = pages[this.state.activeSlide]
     // const { } = this.props;
     // const { } = this.state;
 
     return (
       <article>
-        <Helmet>
-          <title>Живой дом</title>
-          <meta name="description" content="Живой дом" />
-          <meta name="keywords" content="Живой дом" />
-        </Helmet>
-        <div>HomePage</div>
+        <Helmet titleTemplate="%s - Живой дом" defaultTitle="Живой дом" />
+        <div className="layout-wrapper">
+          <LeftSidebar
+            items={mainMenuItems}
+            onClick={this.onMenuClick}
+            activeSlide={this.state.activeSlide}
+            className="left-sidebar"
+          />
+          <div className="region-content">
+            <Page />
+          </div>
+        </div>
       </article>
     );
   }
