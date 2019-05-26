67b32195fda8fd87a4457087fa9b8a76514dac52
markeloff <markeloffds>
2019-05-26 16:32:56 +0600

    has been added profile

--- app/containers/Profile/index.js
+++ app/containers/Profile/index.js
@@ -6,6 +6,13 @@ import axios from 'axios';
 
 import CloseIcon from '../../components/Header/img/close.svg';
 import './Profile.scss';
+import _ from "lodash";
+import classNames from "classnames";
+
+const menuItems = [
+  'МОЙ ПРОФИЛЬ',
+  'ПОКАЗАНИЯ И СЧЕТА'
+]
 
 export default class Profile extends React.Component {
   constructor(props) {
@@ -52,39 +59,98 @@ export default class Profile extends React.Component {
   render() {
 
     return (
-      <div className="profile-page">
-        <button onClick={this.handleClick}>Оплатить водопровод</button>
-        <Dialog
-          onClose={this.handleClose}
-          open={this.state.isPaymentForm}
-          PaperProps={{
-            className: 'popup',
-            style: {
-              maxWidth: '100%',
-              marginLeft: '0',
-              marginRight: '0',
-            },
-          }}
-        >
-          <DialogTitle>
-            <a href="/" className="close" onClick={this.handleClose}>
-              <img src={CloseIcon} alt="close" className="close_img" />
-            </a>
-          </DialogTitle>
-          <DialogContent>
-            <div className="pay_iframe_wrapper">
-              <iframe
-                title="paymentFrame"
-                src="http://18.188.91.108/payWater"
-                // TODO: second button - src="http://18.188.91.108/payEnergy"
-                width="100%"
-                height="100%"
-                align="left"
-              />
+    <div className="service-page">
+      <ul className="service-menu">
+        {_.map(menuItems, (item, key) => (
+          <li
+            key={key}
+            className={classNames([
+              'service-menu-item',
+              { active: this.state.activeSlide === key },
+            ])}
+            onClick={this.onMenuClick}
+            data-key={key}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div className="service-lists">
+        <ul className="service-list">
+          <li onClick={this.handleClick} className="box-item service-item-wrapper">
+            <div className="service-item">
+              <div className="service-item-title-wrapper">
+                <div className="service-item-title">Май 2019</div>
+                <div className="service-item-cost-red">155.0 ₽</div>
+              </div>
+              <div className="service-item-description">
+                Водоснабжение, электроэнергия, ремонт дома
+              </div>
+              <div className="service-item-pay">
+                ОПЛАТИТЬ
+              </div>
             </div>
-          </DialogContent>
-        </Dialog>
+          </li>
+          <Dialog
+            onClose={this.handleClose}
+            open={this.state.isPaymentForm}
+            PaperProps={{
+              className: 'popup',
+              style: {
+                maxWidth: '100%',
+                marginLeft: '0',
+                marginRight: '0',
+              },
+            }}
+          >
+            <DialogTitle>
+              <a href="/" className="close" onClick={this.handleClose}>
+                <img src={CloseIcon} alt="close" className="close_img" />
+              </a>
+            </DialogTitle>
+            <DialogContent>
+              <div className="pay_iframe_wrapper">
+                <iframe
+                  title="paymentFrame"
+                  src="http://18.188.91.108/payWater"
+                  width="100%"
+                  height="100%"
+                  align="left"
+                />
+              </div>
+            </DialogContent>
+          </Dialog>
+          <li className="box-item service-item-wrapper">
+            <div className="service-item">
+              <div className="service-item-title-wrapper">
+                <div className="service-item-title">Апрель 2019</div>
+                <div className="service-item-cost-green">2050.55 ₽</div>
+              </div>
+              <div className="service-item-description">
+                Водоснабжение, электроэнергия, ремонт дома
+              </div>
+              <div className="service-item-pay">
+                ОПЛАЧЕНО
+              </div>
+            </div>
+          </li>
+          <li className="box-item service-item-wrapper">
+            <div className="service-item">
+              <div className="service-item-title-wrapper">
+                <div className="service-item-title">Март 2019</div>
+                <div className="service-item-cost-green">1080.90 ₽</div>
+              </div>
+              <div className="service-item-description">
+                Водоснабжение, электроэнергия, ремонт дома
+              </div>
+              <div className="service-item-pay">
+                ОПЛАЧЕНО
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
+    </div>
     );
   }
 }
